8c05e76c5442e3646f2bb467bfc7c1d5223781db
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-02-23 12:01:14 +0200

    feat: :bug: Fixed API response error
    Checked if the response can be parsed to know if the response from the API has been successful.

--- api.js
+++ api.js
@@ -1,34 +1,51 @@
-import { loadWeatherForecast } from "./domManipulation";
-
-export default function fetchWeatherForecast(location, lat, long) {
+export default function fetchWeatherForecast(
+  location,
+  lat,
+  long,
+  callbackSuccess,
+  callbackFailure,
+  callbackFinally
+) {
   const callURL = `http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`;
 
-  // Showing the loader for the popup first.
-  if (location === "custom") {
-    document.getElementById("popup-loading-container").style.visibility = "visible";
-    document.getElementById("popup-loading-container").style.display = "flex";
-    document.getElementById("popup-forecast-container").style.visibility = "hidden";
-    document.getElementById("popup-forecast-container").style.display = "none";
-  }
-
   fetch(callURL)
-    .then((response) => response.json())
-    .then((jsonResponse) => {
-      // Validating response
-      if (!jsonResponse?.dataseries) {
-        throw new Error("Invalid response");
-      }
-      const forecastResults = jsonResponse.dataseries;
-      // Showing the forecast after everything has been loaded.
-      loadWeatherForecast(location, forecastResults);
-
-      if (location === "custom") {
-        document.getElementById("popup-loading-container").style.visibility = "hiddem";
-        document.getElementById("popup-loading-container").style.display = "none";
-        document.getElementById("popup-forecast-container").style.visibility = "visible";
-        document.getElementById("popup-forecast-container").style.display = "flex";
+    .then((response) => {
+      if (response.headers.get("content-type") === "text/plain") {
+        return response.text();
+      } else {
+        return response.json();
       }
     })
-    .catch((error) => console.error(error))
-    .finally(() => console.log("Network call has been completed."));
+    .then((jsonResponse) => {
+      try {
+        const jsonValue = JSON.parse(jsonResponse);
+
+        // Validating response
+        if (!jsonValue?.dataseries) {
+          throw new Error(`Invalid response from the API: "${jsonValue}"`);
+        }
+        const forecastResults = jsonValue.dataseries;
+        console.log(forecastResults);
+        if (callbackSuccess) {
+          callbackSuccess(location, forecastResults);
+        }
+      } catch (error) {
+        throw new Error(
+          `Invalid response from the API: "${jsonResponse}"`,
+          error
+        );
+      }
+    })
+    .catch((error) => {
+      if (callbackFailure) {
+        callbackFailure(location);
+      }
+      console.error("CATCH", error);
+    })
+    .finally(() => {
+      if (callbackFinally) {
+        callbackFinally();
+      }
+      console.log("Network call has been completed.");
+    });
 }

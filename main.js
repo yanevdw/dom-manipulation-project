061c489ffaf6e6c467a1831f5e728b147f1595c5
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-02-25 15:20:43 +0200

    feat: :recycle: Removed all DOM manipulation and unneccesary imports after removal.
    Removed closePopup and handClick function as well as general implementation involving DOM manipulation.

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
+import "leaflet/dist/leaflet.css";
 import fetchWeatherForecast from "./api";
 import {
   loadWeatherForecast,
   failedForecast,
   hideForecastLoader,
-} from "./domManipulation";
-import {
-  loadMap,
+  closePopup,
+  displayForecastLoader,
   handleMapClick,
-  loadMarker,
-} from "./map";
+  handleMainCityClick,
+} from "./domManipulation";
+import { getMap, resetMap } from "./map";
 
-// Toggle visibility of the pop-up
-const mainLocations = document.getElementById("main-cities-container");
-const customLocationPopup = document.getElementById("popup-container");
-mainLocations.style.visibility = "visible";
-customLocationPopup.style.visibility = "hidden";
+// Only a loader for the pop-up. Ensures it only displays if results haven't been returned yet.
+displayForecastLoader("custom");
+
+// Get the parent element for the main cities/locations which holds all the buttons.
+const mainCities = document.getElementById("main-cities-nav");
+// Get all the button elements in this parent.
+const mainCityButtons = mainCities.getElementsByClassName("city-chip");
+// Add a click event to each of the main city chips to show their respective forecasts.
+for (let i = 0; i < mainCityButtons.length; i++) {
+  mainCityButtons[i].addEventListener("click", () => handleMainCityClick(i));
+}
 
 // Complete all the network calls for the main cities.
 // Network call for Pretoria
@@ -55,83 +62,18 @@ fetchWeatherForecast(
 );
 
 // Set the initial view of the map to that of South Africa.
-const map = loadMap();
+const map = getMap();
 
-// Get the parent element for the main cities/locations which holds all the buttons.
-const mainCities = document.getElementById("main-cities-nav");
-// Get all the button elements in this parent.
-const mainCityButtons = mainCities.getElementsByClassName("city-chip");
-// Set the default city to the first option.
-let selectedMainCity = 0;
-// Make Pretoria the  default main city displayed on load.
-mainCityButtons[selectedMainCity].classList.add("active");
-document.getElementById("pretoria-container").style.visibility = "visible";
-// Hide all other main cities.
-document.getElementById("johannesburg-container").style.visibility = "hidden";
-document.getElementById("durban-container").style.visibility = "hidden";
-document.getElementById("cape-town-container").style.visibility = "hidden";
+// Display the forecast for Pretoria as default.
+handleMainCityClick(0);
 
 // Add a click event to the map to process when a custom or random location is selected on the map.
 map.on("click", handleMapClick);
 
-// Controls what happens each time a new main city is clicked.
-function handleClick(numButton) {
-  const forecastDisplay = document.getElementsByClassName("city-forecast");
-  // Remove the active state from the previously selected button.
-  mainCityButtons[selectedMainCity].classList.remove("active");
-  // Update the index of the newly/currently selected button.
-  selectedMainCity = numButton;
-  // Set the newly selected button to active.
-  mainCityButtons[selectedMainCity].classList.add("active");
-
-  // Display forecast for selected city and hide forecasts for other main cities/locations.
-  for (let j = 0; j < forecastDisplay.length; j++) {
-    if (j === selectedMainCity) {
-      // Display corresponding forecast of selected main city.
-      forecastDisplay[j].style.visibility = "visible";
-      forecastDisplay[j].style.display = "flex";
-    } else {
-      // Hide all other cities' information.
-      forecastDisplay[j].style.visibility = "hidden";
-      forecastDisplay[j].style.display = "none";
-    }
-  }
-
-  // Update the location of the marker.
-  if (selectedMainCity === 0) {
-    loadMarker(-25.73134, 28.21837);
-  } else if (selectedMainCity === 1) {
-    loadMarker(-26.195246, 28.034088);
-  } else if (selectedMainCity === 2) {
-    loadMarker(-29.8579, 31.0292);
-  } else if (selectedMainCity === 3) {
-    loadMarker(-33.918861, 18.4233);
-  }
-}
-
-// Add a click event to each of the main city chips to show their respective forecasts.
-for (let i = 0; i < mainCityButtons.length; i++) {
-  mainCityButtons[i].addEventListener("click", () => handleClick(i));
-}
-
-// Close the popup when the close icon is selected.
-function closePopup() {
-  mainLocations.style.visibility = "visible";
-  mainLocations.style.display = "flex";
-  customLocationPopup.style.visibility = "hidden";
-  customLocationPopup.style.display = "none";
-
-  if (selectedMainCity === 0) {
-    loadMarker(-25.73134, 28.21837);
-  } else if (selectedMainCity === 1) {
-    loadMarker(-26.195246, 28.034088);
-  } else if (selectedMainCity === 2) {
-    loadMarker(-29.8579, 31.0292);
-  } else if (selectedMainCity === 3) {
-    loadMarker(-33.918861, 18.4233);
-  }
-}
-
 // Add the click event to the close icon to trigger the close function.
 const closePopupButton = document.getElementById("close-popup-button");
 closePopupButton.addEventListener("click", () => closePopup());
+
+// Add the click event to the reset button to trigger the reset fucnction.
+const resetMapButton = document.getElementById("reset-map-button");
+resetMapButton.addEventListener("click", () => resetMap());

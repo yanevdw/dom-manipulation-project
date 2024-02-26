836246fffe0f72ca242e68d47363b0ff02f043b1
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-02-26 13:39:43 +0200

    feat: :bug: Fixed marker display on build bug
    Created a default icon to indicate the marker for the map

--- map.js
+++ map.js
@@ -1,8 +1,18 @@
 import L from "leaflet";
 
+const markerIcon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+
+  iconSize: [25, 41],
+  shadowSize: [40, 64],
+  iconAnchor: [13, 40],
+  shadowAnchor: [10, 62],
+});
+
 const map = L.map("map").setView([-28.921631, 25.224609], 4);
 // Set the initial marker location ot that of Pretoria as this is the default.
-const marker = L.marker([-25.73134, 28.21837]).addTo(map);
+const marker = L.marker([-25.73134, 28.21837], { icon: markerIcon }).addTo(map);
 
 export function getMap() {
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {

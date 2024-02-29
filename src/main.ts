eb4a99d1641cbbac5c7081ebe795f2a029c40a6e
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-02-29 16:58:00 +0200

    feat: :art: Formatted file and edited implementation to include RxJS and Observables.
    Removed unneccessary imports, added handleFetch function which handles the subject and also susbrcibed the two observables to allow netwrok calls for both main cities and custom locations.

--- src/main.ts
+++ src/main.ts
@@ -1,84 +1,94 @@
 import "leaflet/dist/leaflet.css";
-import fetchWeatherForecast from "./api";
 import {
   loadWeatherForecast,
-  failedForecast,
-  hideForecastLoader,
   closePopup,
   displayForecastLoader,
   handleMainCityClick,
-  fetchPopup,
+  togglePopup,
+  hideForecastLoader,
 } from "./domManipulation";
-import { getMap, resetMap, bindMapClick, loadMarker } from "./map";
+import {
+  getMap,
+  resetMap,
+  customLat,
+  customLong,
+  bindMapClick,
+  loadMarker,
+} from "./map";
+import {
+  signalNewForecastFetch$,
+  fetchResultSet$,
+  fetchCustomResultSet$,
+} from "./api";
 
-// Only a loader for the pop-up. Ensures it only displays if results haven't been returned yet.
-displayForecastLoader("custom");
+let location = "pretoria";
+
+function handleFetch(loc: string, lat: string, long: string) {
+  location = loc;
+  displayForecastLoader("custom");
+  signalNewForecastFetch$.next([lat, long]);
+}
+
+function handleCityClick(selectedCity: number) {
+  if (selectedCity === 0) {
+    handleFetch("pretoria", "-25.731340", "28.218370");
+    handleMainCityClick(selectedCity, loadMarker);
+  } else if (selectedCity === 1) {
+    handleFetch("johannesburg", "-26.195246", "28.034088");
+    handleMainCityClick(selectedCity, loadMarker);
+  } else if (selectedCity === 2) {
+    handleFetch("durban", "-29.85790000", "31.02920000");
+    handleMainCityClick(selectedCity, loadMarker);
+  } else if (selectedCity === 3) {
+    handleFetch("cape-town", "-33.918861", "18.423300");
+    handleMainCityClick(selectedCity, loadMarker);
+  }
+}
+
+function handleMapClick() {
+  handleFetch("custom", customLat, customLong);
+  fetchCustomResultSet$.subscribe((result) => {
+    loadWeatherForecast(location, result.result.dataseries);
+  });
+
+  togglePopup();
+  hideForecastLoader("custom");
+}
+
+fetchResultSet$.subscribe((result) => {
+  loadWeatherForecast(location, result.result.dataseries);
+});
+// // Only a loader for the pop-up. Ensures it only displays if results haven't been returned yet.
+// displayForecastLoader("custom");
 
 // Get the parent element for the main cities/locations which holds all the buttons.
 const mainCities = document.getElementById("main-cities-nav");
 // Get all the button elements in this parent.
-if (mainCities){
+if (mainCities) {
   const mainCityButtons = mainCities.getElementsByClassName("city-chip");
   // Add a click event to each of the main city chips to show their respective forecasts.
   for (let i = 0; i < mainCityButtons.length; i++) {
-    mainCityButtons[i].addEventListener("click", () => handleMainCityClick(i, loadMarker));
+    mainCityButtons[i].addEventListener("click", () => handleCityClick(i));
   }
 }
 
-
-// Complete all the network calls for the main cities.
-// Network call for Pretoria
-fetchWeatherForecast(
-  "pretoria",
-  "-25.731340",
-  "28.218370",
-  loadWeatherForecast,
-  failedForecast,
-  hideForecastLoader,
-);
-// Network call for Johannesburg
-fetchWeatherForecast(
-  "johannesburg",
-  "-26.195246",
-  "28.034088",
-  loadWeatherForecast,
-  failedForecast,
-  hideForecastLoader,
-);
-// Network call for Durban
-fetchWeatherForecast(
-  "durban",
-  "-29.85790000",
-  "31.02920000",
-  loadWeatherForecast,
-  failedForecast,
-  hideForecastLoader,
-);
-// Network call Cape Town
-fetchWeatherForecast(
-  "cape-town",
-  "-33.918861",
-  "18.423300",
-  loadWeatherForecast,
-  failedForecast,
-  hideForecastLoader,
-);
-
 getMap();
+
+// // Display the forecast for Pretoria as default.
+handleCityClick(0);
+
 // Add a click event to the map to process when a custom or random location is selected on the map.
-bindMapClick(fetchPopup);
-
-// Display the forecast for Pretoria as default.
-handleMainCityClick(0, loadMarker);
-
-// Add the click event to the close icon to trigger the close function.
-// Add the click event to the reset button to trigger the reset fucnction.
+bindMapClick(handleMapClick);
+// // Add the click event to the close icon to trigger the close function.
+// // Add the click event to the reset button to trigger the reset fucnction.
 const closePopupButton = document.getElementById("close-popup-button");
 const resetMapButton = document.getElementById("reset-map-button");
+
+fetchResultSet$.subscribe((result) => {
+  loadWeatherForecast(location, result.result.dataseries);
+});
 
 if (closePopupButton && resetMapButton) {
   closePopupButton.addEventListener("click", () => closePopup(loadMarker));
   resetMapButton.addEventListener("click", () => resetMap());
 }
-
-

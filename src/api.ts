95d9ee025c203cb1c6e6006c950edd8e06327b71
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-02-29 17:09:40 +0200

    feat: :pencil2: Updated API response interface name for case sensitivity
    weatherResponse to WeatherResponse as in the type files.

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-import { weatherResponse } from "./types/customTypes";
+import { WeatherResponse } from "./types/customTypes";
 import { EMPTY, Subject, catchError, map, switchMap, throttleTime } from "rxjs";
 import { fromFetch } from "rxjs/fetch";
 import { fromPromise } from "rxjs/internal/observable/innerFrom";
@@ -11,7 +11,7 @@ export const fetchResultSet$ = signalNewForecastFetch$.pipe(
       `http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`
     ).pipe(
       switchMap((res) => fromPromise(res.json())),
-      map((json: weatherResponse) => ({
+      map((json: WeatherResponse) => ({
         result: json,
       })),
       catchError((error) => {
@@ -29,7 +29,7 @@ export const fetchCustomResultSet$ = signalNewForecastFetch$.pipe(
       `http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`
     ).pipe(
       switchMap((res) => fromPromise(res.json())),
-      map((json: weatherResponse) => ({
+      map((json: WeatherResponse) => ({
         result: json,
       })),
       catchError((error) => {

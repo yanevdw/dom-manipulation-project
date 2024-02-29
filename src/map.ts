2e468a07d748c721d5fb9373317d3713c3e8bc41
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-02-29 17:00:42 +0200

    feat: :art: Updated bindMapClick()
    Set the selected latitutde and longitude of the selected location to the variables declared above to allow the subject and observable in main to process the network call using these variables.

--- src/map.ts
+++ src/map.ts
@@ -1,5 +1,7 @@
 import L, { LatLng } from "leaflet";
 
+export let customLat: string;
+export let customLong: string;
 
 const markerIcon = L.icon({
   iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
@@ -14,31 +16,31 @@ const markerIcon = L.icon({
 const map = L.map("map").setView([-28.921631, 25.224609], 4);
 // Set the initial marker location ot that of Pretoria as this is the default.
 
-
 const marker = L.marker([-25.73134, 28.21837], { icon: markerIcon }).addTo(map);
 
 export function getMap() {
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution:
-      "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>",
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
-
 }
 
 // Controls what happens when user clicks on the map to select own location.
-export function bindMapClick( callbackMapClick : (lat: string, long: string) => void){ 
-  map.addEventListener("click", (mapClickEvent: { latlng:  LatLng }) => {
+export function bindMapClick(callbackMapClick: () => void) {
+  map.addEventListener("click", (mapClickEvent: { latlng: LatLng }) => {
     const latLong = String(mapClickEvent.latlng.wrap());
-    const lat = latLong.substring(latLong.indexOf("(") + 1, latLong.indexOf(","));
-    const long = latLong.substring(
-      latLong.indexOf(",") + 2,
-      latLong.indexOf(")"),
+    customLat = latLong.substring(
+      latLong.indexOf("(") + 1,
+      latLong.indexOf(",")
     );
-    loadMarker(Number(lat), Number(long));
-    callbackMapClick(lat, long);
+    customLong = latLong.substring(
+      latLong.indexOf(",") + 2,
+      latLong.indexOf(")")
+    );
+    loadMarker(Number(customLat), Number(customLong));
+    callbackMapClick();
   });
-
 }
 
 export function loadMarker(lat: number, long: number) {

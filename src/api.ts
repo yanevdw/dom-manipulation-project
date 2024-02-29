fcddd530c833cdaca2ef8c5e76899e2a50e64dd4
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-02-29 16:52:29 +0200

    feat: :sparkles: Added RxJS and observables to API calls
    Edited API calls to use RxJS and observables instead.

--- src/api.ts
+++ src/api.ts
@@ -1,53 +1,41 @@
-import { Forecast } from "./types/customTypes";
+import { weatherResponse } from "./types/customTypes";
+import { EMPTY, Subject, catchError, map, switchMap, throttleTime } from "rxjs";
+import { fromFetch } from "rxjs/fetch";
+import { fromPromise } from "rxjs/internal/observable/innerFrom";
 
-export default function fetchWeatherForecast(
-  location: string,
-  lat: string,
-  long: string,
-  callbackSuccess: (loc: string, res: Forecast[]) => void,
-  callbackFailure: (loc: string) => void,
-  callbackFinally: (loc: string) => void,
-) {
-  const callURL = `http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`;
+export const signalNewForecastFetch$ = new Subject<[string, string]>();
+export const fetchResultSet$ = signalNewForecastFetch$.pipe(
+  throttleTime(200),
+  switchMap(([lat, long]) =>
+    fromFetch(
+      `http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`
+    ).pipe(
+      switchMap((res) => fromPromise(res.json())),
+      map((json: weatherResponse) => ({
+        result: json,
+      })),
+      catchError((error) => {
+        console.error("JSON Parse Fail", error);
+        return EMPTY;
+      })
+    )
+  )
+);
 
-  fetch(callURL)
-    .then((response) => {
-      if (response.headers.get("content-type") === "text/plain") {
-        return response.text();
-      }
-      return response.json();
-    })
-    .then((jsonResponse) => {
-      try {
-        let jsonValue = jsonResponse;
-        if (
-          typeof jsonResponse === "string"
-        ) {
-          jsonValue = JSON.parse(jsonResponse);
-        }
-        // Validating response
-        if (!jsonValue?.dataseries) {
-          throw new Error(`Invalid response from the API: "${jsonValue}"`);
-        }
-        const forecastResults = jsonValue.dataseries;
-        if (callbackSuccess) {
-          callbackSuccess(location, forecastResults);
-        }
-      } catch (error) {
-        throw new Error(
-          `Invalid response from the API: "${jsonResponse}"`
-        );
-      }
-    })
-    .catch((error) => {
-      if (callbackFailure) {
-        callbackFailure(location);
-      }
-      console.error("CATCH", error);
-    })
-    .finally(() => {
-      if (callbackFinally) {
-        callbackFinally(location);
-      }
-    });
-}
+export const fetchCustomResultSet$ = signalNewForecastFetch$.pipe(
+  throttleTime(1500),
+  switchMap(([lat, long]) =>
+    fromFetch(
+      `http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`
+    ).pipe(
+      switchMap((res) => fromPromise(res.json())),
+      map((json: weatherResponse) => ({
+        result: json,
+      })),
+      catchError((error) => {
+        console.error("JSON Parse Fail", error);
+        return EMPTY;
+      })
+    )
+  )
+);

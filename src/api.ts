f6853996e57f1be15791f54b5cc3a5e32cac17fc
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-03-01 10:21:34 +0200

    feat: :fire: Removed second observable
    Only required one observable/subscriber, hecne fetchCustomResultSet was removed

--- src/api.ts
+++ src/api.ts
@@ -4,26 +4,9 @@ import { fromFetch } from "rxjs/fetch";
 import { fromPromise } from "rxjs/internal/observable/innerFrom";
 
 export const signalNewForecastFetch$ = new Subject<[string, string]>();
+
 export const fetchResultSet$ = signalNewForecastFetch$.pipe(
   throttleTime(200),
-  switchMap(([lat, long]) =>
-    fromFetch(
-      `http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`
-    ).pipe(
-      switchMap((res) => fromPromise(res.json())),
-      map((json: WeatherResponse) => ({
-        result: json,
-      })),
-      catchError((error) => {
-        console.error("JSON Parse Fail", error);
-        return EMPTY;
-      })
-    )
-  )
-);
-
-export const fetchCustomResultSet$ = signalNewForecastFetch$.pipe(
-  throttleTime(1500),
   switchMap(([lat, long]) =>
     fromFetch(
       `http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`
